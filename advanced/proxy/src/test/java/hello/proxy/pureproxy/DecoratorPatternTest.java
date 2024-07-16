b24c144c3d3354d4ef2bea4c43fdee9c24f64459
SHEOMM <esh0821@snu.ac.kr>
2024-07-16 20:37:05 +0900

    feat: Decorator 패턴 연습 2

--- advanced/proxy/src/test/java/hello/proxy/pureproxy/DecoratorPatternTest.java
+++ advanced/proxy/src/test/java/hello/proxy/pureproxy/DecoratorPatternTest.java
@@ -13,4 +13,22 @@ public class DecoratorPatternTest {
                 DecoratorPatternClient(realComponent);
         client.execute();
     }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new
+                DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
 }
